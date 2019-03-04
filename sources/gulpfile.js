320548712ef0922790fcd18edf8381c866049a2b
Sven Eberth <se@mausbrand.de>
2019-03-04 18:11:20 +0100

    .editorconfig, npm dependencies/gulp updated

--- sources/gulpfile.js
+++ sources/gulpfile.js
@@ -1,15 +1,15 @@
 // Gulp Vi
 
 var srcpaths = {
-  less: './less/**/*.less',
-  images: './images/**/*',
-  embedsvg: './embedsvg/**/*',
+	less: './less/**/*.less',
+	images: './images/**/*',
+	embedsvg: './embedsvg/**/*',
 };
 
 var destpaths = {
-  css: '../public/css',
-  images: '../public/images',
-  embedsvg: '../public/embedsvg',
+	css: '../public/css',
+	images: '../public/images',
+	embedsvg: '../public/embedsvg',
 };
 
 // Variables and requirements
@@ -37,94 +37,92 @@ const cheerio = require('gulp-cheerio');
 gulp.task('clean', function () {
 	return del([
 		destpaths.css + '/**/*',
-		destpaths.images+ '/**/*',
+		destpaths.images + '/**/*',
 		destpaths.embedsvg + '/**/*',
 	], {force: true});
 });
 
 // compilation and postproduction of LESS to CSS
 gulp.task('css', function () {
-  //gulp.start('dev')
-  var processors = [
-    nocomments, // discard comments
-    focus, // add focus to hover-states
-    zindex, // reduce z-index values
-  ];
-  return gulp.src('./less/vi.less')
-    .pipe(less({
-      paths: [path.join(__dirname, 'less', 'includes')]
-    })) // compile less to css
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    })) // add vendor prefixes
-    .pipe(postcss(processors)) // clean up css
-    .pipe(jmq({
-      log: true
-    }))
-    .pipe(stylefmt()) // syntax formatting
-    .pipe(rename('style.css'))
-    .pipe(gulp.dest(destpaths.css)) // save cleaned version
-    .pipe(nano()) // minify css
-    .pipe(rename('style.min.css')) // save minified version
-    .pipe(gulp.dest(destpaths.css));
+	//gulp.start('dev')
+	var processors = [
+		nocomments, // discard comments
+		focus, // add focus to hover-states
+		zindex, // reduce z-index values
+	];
+	return gulp.src('./less/vi.less')
+		.pipe(less({
+			paths: [path.join(__dirname, 'less', 'includes')]
+		})) // compile less to css
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		})) // add vendor prefixes
+		.pipe(postcss(processors)) // clean up css
+		.pipe(jmq())
+		.pipe(stylefmt()) // syntax formatting
+		.pipe(rename('style.css'))
+		.pipe(gulp.dest(destpaths.css)) // save cleaned version
+		.pipe(nano()) // minify css
+		.pipe(rename('style.min.css')) // save minified version
+		.pipe(gulp.dest(destpaths.css));
 });
 
 // reduce images for web
 gulp.task('images', function () {
-  return gulp.src(srcpaths.images)
-    .pipe(imagemin([
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-        plugins: [
-          {removeViewBox: false},
-          {removeDimensions: true}
-        ]
-      })
-    ]))
-    .pipe(gulp.dest(destpaths.images));
+	return gulp.src(srcpaths.images)
+		.pipe(imagemin([
+			imagemin.jpegtran({progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+					{removeViewBox: false},
+					{removeDimensions: true}
+				]
+			})
+		]))
+		.pipe(gulp.dest(destpaths.images));
 });
 
 gulp.task('embedsvg', function () {
-  return gulp.src(srcpaths.embedsvg)
-    .pipe(imagemin([
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-        plugins: [
-          {removeViewBox: false},
-          {removeDimensions: true}
-        ]
-      })
-    ]))
-    .pipe(cheerio({
-      run: function ($, file) {
-        $('style').remove()
-        $('title').remove()
-        $('[id]').removeAttr('id')
-        //$('[class]').removeAttr('class')
-        //$('[fill]').removeAttr('fill')
-        $('svg').addClass('icon')
-      },
-      parserOptions: {xmlMode: true}
-    }))
-    .pipe(rename(function (path) {
-      if (path.extname) {
-        if (path.dirname === '.') {
-          path.dirname = '';
-        }
-        path.basename = path.dirname + '-' + path.basename;
-        path.dirname = '';
-      }
-    }))
-    .pipe(gulp.dest(destpaths.embedsvg));
+	return gulp.src(srcpaths.embedsvg)
+		.pipe(imagemin([
+			imagemin.jpegtran({progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+					{removeViewBox: false},
+					{removeDimensions: true}
+				]
+			})
+		]))
+		.pipe(cheerio({
+			run: function ($, file) {
+				$('style').remove();
+				$('title').remove();
+				$('[id]').removeAttr('id')
+				//$('[class]').removeAttr('class')
+				//$('[fill]').removeAttr('fill')
+				$('svg').addClass('icon')
+			},
+			parserOptions: {xmlMode: true}
+		}))
+		.pipe(rename(function (path) {
+			if (path.extname) {
+				if (path.dirname === '.') {
+					path.dirname = '';
+				}
+				path.basename = path.dirname + '-' + path.basename;
+				path.dirname = '';
+			}
+		}))
+		.pipe(gulp.dest(destpaths.embedsvg));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(srcpaths.less, gulp.series(['css']));
-  gulp.watch(srcpaths.embedsvg, gulp.series(['embedsvg']));
-  gulp.watch(srcpaths.images, gulp.series(['images']));
+	gulp.watch(srcpaths.less, gulp.series(['css']));
+	gulp.watch(srcpaths.embedsvg, gulp.series(['embedsvg']));
+	gulp.watch(srcpaths.images, gulp.series(['images']));
 });
 
 gulp.task('default', gulp.series(['clean', 'css', 'embedsvg', 'images']));
