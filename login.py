be177892c51a4b3559694bc5fcc65b153dcea999
Jan Max Meyer <jmm@phorward.de>
2019-08-08 14:50:03 +0200

    Logo path

--- login.py
+++ login.py
@@ -72,7 +72,7 @@ class BaseLoginHandler(html5.Li):
 		# Logo
 		img = html5.Img()
 		img.addClass("vi-login-logo")
-		img["src"] = "images/vi-login-logo.svg"
+		img["src"] = "vi/public/images/vi-login-logo.svg"
 		self.loginHeader.appendChild(img)
 
 
