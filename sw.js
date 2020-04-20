e7adf3f91be121e79170f0d578a31068dee77eaf
Andreas H. Kelch <ak@mausbrand.de>
2020-04-20 06:46:37 +0200

    installable vi, Versionnumber in version.json must be increased to update the service worker cache

--- sw.js
+++ sw.js
@@ -1,16 +1,39 @@
+
+
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-		return cache.addAll([
-	      '/vi/s/pyodide/pyodide.asm.data',
-	      '/vi/s/pyodide/pyodide.asm.wasm',
-	      '/vi/s/pyodide/pyodide.asm.js',
-	      '/vi/s/pyodide/pyodide.asm.data.js',
-	      '/vi/s/pyodide/pyodide.js',
-		  '/vi/s/main.html'
-        ])
-    })
-  );
+		event.waitUntil(
+			fetch('version.json')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				let versionInfo = data;
+				console.log(data)
+				let currentCacheName = "sw-cache-"+data["version"];
+
+				caches.keys().then(function(names) {
+				    for (let name of names)
+				    	if (name != currentCacheName){
+				    		caches.delete(name);
+					    }
+
+				}).then(()=>{
+					caches.open(currentCacheName).then(function(cache) {
+					return cache.addAll([
+				      '/vi/s/pyodide/pyodide.asm.data',
+				      '/vi/s/pyodide/pyodide.asm.wasm',
+				      '/vi/s/pyodide/pyodide.asm.js',
+				      '/vi/s/pyodide/pyodide.asm.data.js',
+				      '/vi/s/pyodide/pyodide.js',
+					  '/vi/s/main.html'
+			        ])
+			    })
+				})
+
+
+			})
+		);
+
 });
 
 self.addEventListener('fetch', function(event) {
@@ -26,3 +49,19 @@ self.addEventListener('fetch', function(event) {
 	    })
     );
 });
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+
+
