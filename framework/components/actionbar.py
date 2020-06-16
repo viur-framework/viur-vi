8265d746fb2418e1ae3b48f53c9880b411ca9300
Tobias Steinrücken <ts@mausbrand.de>
2020-06-16 15:18:32 +0200

    First implementation of purely server defined actions
    Work-in-Progress; serversideaction.py should probably reside in vi/actions
    but this is currently not possible due to circular import
    (NetworkService -> framework (components/actionbar) -> actions -> NetworkService)

--- framework/components/actionbar.py
+++ framework/components/actionbar.py
@@ -4,6 +4,7 @@ from vi import html5
 from vi.priorityqueue import actionDelegateSelector
 from vi.config import conf
 from vi.i18n import translate
+from vi.serversideaction import ServerSideActionWdg
 
 class ActionBar(html5.Div):
 	"""
@@ -64,6 +65,16 @@ class ActionBar(html5.Div):
 						actionWdg.postInit(widget=widget)
 
 					self.widgets[ action ] = actionWdg
+				else:  # We may have a server-defined action
+					print("llll1")
+					if handler and "customActions" in conf["modules"][self.module]:
+						print("llll2")
+						if action in conf["modules"][self.module]["customActions"]:
+							print("llll3")
+							actionWdg = ServerSideActionWdg(self.module, handler, action,
+														conf["modules"][self.module]["customActions"][action])
+							self.appendChild(actionWdg)
+							self.widgets[action] = actionWdg
 
 	def getActions(self):
 		"""
