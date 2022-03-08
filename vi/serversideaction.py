737261c0e283d1236430890ff86d815a2895cdb6
Andreas H. Kelch <ak@mausbrand.de>
2022-03-08 14:26:53 +0100

    feat: serversideactions now can also be not selection sensitive

--- vi/serversideaction.py
+++ vi/serversideaction.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from vi.config import conf
-from flare.html5 import window
-from flare.button import Button
-from flare.safeeval import SafeEval
-from flare.network import NetworkService
 import logging
+
+from flare.button import Button
+from flare.html5 import window
+from flare.network import NetworkService
+from flare.safeeval import SafeEval
+
+from vi.config import conf
 
 
 class ServerSideActionWdg(Button):
 	def __init__(self, module, handler, actionName, actionData):
-		super(ServerSideActionWdg, self).__init__(actionData["name"])
+		super(ServerSideActionWdg, self).__init__(actionData["name"], icon=actionData["icon"])
 		self.module = module
 		self.handler = handler
 		self.actionName = actionName
@@ -49,6 +51,10 @@ class ServerSideActionWdg(Button):
 		super(ServerSideActionWdg, self).onDetach()
 
 	def onSelectionChanged(self, table, selection, *args, **kwargs):
+		if self.actionData["enabled"] == "True":
+			self.switchDisabledState(False)
+			return
+
 		if self.actionData["allowMultiSelection"] and len(selection) > 0 or len(selection) == 1:
 			if not self.pendingFetches:
 				if self.selectionCheckerAst:
@@ -56,7 +62,7 @@ class ServerSideActionWdg(Button):
 					for sel in selection:
 						try:
 							if not self.se.execute(self.selectionCheckerAst,
-													{"skel": sel, "additionalEvalData": self.additionalEvalData}):
+												   {"skel": sel, "additionalEvalData": self.additionalEvalData}):
 								valid = False
 								break
 						except Exception as e:
@@ -86,6 +92,19 @@ class ServerSideActionWdg(Button):
 			if wasIdle and self.pendingFetches:
 				self.addClass("is-loading")
 				self.fetchNext()
+		elif self.actionData["enabled"] == "True":
+			wasIdle = not self.pendingFetches
+			if self.actionData["action"] == "open":
+				url = self.actionData["url"]
+				window.open(url, "_blank")
+			elif self.actionData["action"] == "fetch":
+				url = self.actionData["url"]
+				self.pendingFetches.append(url)
+			else:
+				raise NotImplementedError()
+			if wasIdle and self.pendingFetches:
+				self.addClass("is-loading")
+				self.fetchNext()
 
 	def fetchNext(self):
 		if not self.pendingFetches:
@@ -101,14 +120,15 @@ class ServerSideActionWdg(Button):
 		if self.pendingFetches:
 			self.fetchNext()
 		else:
-			conf["mainWindow"].log("success", "Done :)")
+			conf["mainWindow"].log("success", "Done")
 
 			self.removeClass("is-loading")
 			NetworkService.notifyChange(self.parent().parent().module)
 
-	def fetchFailed(self, req,code):
+	def fetchFailed(self, req, code):
 		self.pendingFetches = []
-		conf["mainWindow"].log("error", "Failed :(")
+		conf["mainWindow"].log("error", "Failed")
+		self.resetLoadingState()
 
 	def resetLoadingState(self):
 		self.removeClass("is-loading")
